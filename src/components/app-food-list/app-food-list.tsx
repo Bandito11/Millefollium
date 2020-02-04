717645474e3b63b54024a5b07326e2036f3a8c94
Esteban Morales <red_bandit_11@hotmail.com>
2020-02-04 16:21:40 -0400

    only get data from db

--- src/components/app-food-list/app-food-list.tsx
+++ src/components/app-food-list/app-food-list.tsx
@@ -25,12 +25,12 @@ export class AppFoodList {
 
     componentWillLoad() {
         this.getFrequentFoodProducts();
-        if (typeof (this.foodDataWorker) == 'undefined') {
-            this.foodDataWorker = new Worker('workers/usda-file.js');
-        }
-        this.foodDataWorker.onmessage = function (event) {
-            usdaData = event.data;
-        };
+//        if (typeof (this.foodDataWorker) == 'undefined') {
+//            this.foodDataWorker = new Worker('workers/usda-file.js');
+//        }
+//       this.foodDataWorker.onmessage = function (event) {
+//            usdaData = event.data;
+//        };
     }
 
     componentDidLoad() {
@@ -55,238 +55,238 @@ export class AppFoodList {
         this.queryByNameOrId(ev.detail.value);
     }
 
-    queryUSDAByName(value) {
-        const foodProduct: IFoodProduct = {
-            name: '',
-            barcode: '',
-            picture: '',
-            servingPerContainer: '1',
-            servingSize: {
-                size: '1',
-                grams: '100',
-                measurement: 'serving',
-            },
-            calories: '0',
-            fat: {
-                total: {
-                    grams: '0',
-                    percent: '0',
-                },
-                saturated: {
-                    grams: '0',
-                    percent: '0',
-                },
-                trans: {
-                    grams: '0',
-                    percent: '0',
-                },
-                polyunsaturated: {
-                    grams: '0',
-                    percent: '0',
-                },
-                monounsaturated: {
-                    grams: '0',
-                    percent: '0',
-                },
-            },
-            cholesterol: {
-                grams: '0',
-                percent: '0',
-            },
-            sodium: {
-                grams: '0',
-                percent: '0',
-            },
-            potassium: {
-                grams: '0',
-                percent: '0',
-            },
-            totalCarbohydrates: {
-                grams: '0',
-                percent: '0',
-            },
-            dietaryFiber: {
-                grams: '0',
-                percent: '0',
-            },
-            protein: {
-                grams: '0',
-                percent: '0',
-            },
-            niacin: {
-                grams: '0',
-                percent: '0',
-            },
-            phosphorus: {
-                grams: '0',
-                percent: '0',
-            },
-            calcium: {
-                grams: '0',
-                percent: '0',
-            },
-            iron: {
-                grams: '0',
-                percent: '0',
-            },
-            magnesium: {
-                grams: '0',
-                percent: '0',
-            },
-            manganese: {
-                grams: '0',
-                percent: '0',
-            },
-            dateCreated: null,
-            vitamin: {
-                A: {
-                    grams: '0',
-                    percent: '0',
-                },
-                B: {
-                    grams: '0',
-                    percent: '0',
-                },
-                C: {
-                    grams: '0',
-                    percent: '0',
-                },
-                D: {
-                    grams: '0',
-                    percent: '0',
-                },
-                E: {
-                    grams: '0',
-                    percent: '0',
-                },
-            },
-            sugar: {
-                added: {
-                    grams: '0',
-                    percent: '0',
-                },
-                total: {
-                    grams: '0',
-                    percent: '0',
-                }
-            },
-            sugarAlcohol: {
-                added: {
-                    grams: '0',
-                    percent: '0',
-                },
-                total: {
-                    grams: '0',
-                    percent: '0',
-                }
-            }
+// queryUSDAByName(value) {
+//     const foodProduct: IFoodProduct = {
+//         name: '',
+//         barcode: '',
+//         picture: '',
+//         servingPerContainer: '1',
+//         servingSize: {
+//             size: '1',
+//             grams: '100',
+//             measurement: 'serving',
+//         },
+//         calories: '0',
+//         fat: {
+//             total: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             saturated: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             trans: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             polyunsaturated: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             monounsaturated: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//         },
+//         cholesterol: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         sodium: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         potassium: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         totalCarbohydrates: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         dietaryFiber: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         protein: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         niacin: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         phosphorus: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         calcium: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         iron: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         magnesium: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         manganese: {
+//             grams: '0',
+//             percent: '0',
+//         },
+//         dateCreated: null,
+//         vitamin: {
+//             A: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             B: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             C: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             D: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             E: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//         },
+//         sugar: {
+//             added: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             total: {
+//                 grams: '0',
+//                 percent: '0',
+//             }
+//         },
+//         sugarAlcohol: {
+//             added: {
+//                 grams: '0',
+//                 percent: '0',
+//             },
+//             total: {
+//                 grams: '0',
+//                 percent: '0',
+//             }
+//         }
 
-        };
-        const usdaProducts: (IFoodProduct)[] = [];
-        usdaData.forEach(product => {
-            if (product.foodName.includes(value)) {
-                usdaProducts.push({
-                    ...foodProduct,
-                    name: product.foodName,
-                    protein: {
-                        percent: '0',
-                        grams: product.protein.toString()
-                    },
-                    fat: {
-                        ...foodProduct.fat,
-                        total: {
-                            percent: '0',
-                            grams: product.fat.toString()
-                        },
-                        saturated: {
-                            percent: '0',
-                            grams: product.saturatedFat.toString()
-                        }
-                    },
-                    totalCarbohydrates: {
-                        percent: '0',
-                        grams: product.carbohydrates.toString()
-                    },
-                    calories: product.calories.toString(),
-                    sugar: {
-                        ...foodProduct.sugar,
-                        total: {
-                            ...foodProduct.sugar.total,
-                            grams: product.sugar.toString()
-                        }
-                    },
-                    dietaryFiber: {
-                        percent: '0',
-                        grams: product.fiber.toString()
-                    },
-                    calcium: {
-                        percent: '0',
-                        grams: product.calcium.toString()
-                    },
-                    iron: {
-                        percent: '0',
-                        grams: product.iron.toString()
-                    },
-                    magnesium: {
-                        percent: '0',
-                        grams: product.magnesium.toString()
-                    },
-                    phosphorus: {
-                        percent: '0',
-                        grams: product.phosphorus.toString()
-                    },
-                    potassium: {
-                        percent: '0',
-                        grams: product.potassium.toString()
-                    },
-                    sodium: {
-                        percent: '0',
-                        grams: product.sodium.toString()
-                    },
-                    manganese: {
-                        percent: '0',
-                        grams: product.manganese.toString()
-                    },
-                    vitamin: {
-                        ...foodProduct.vitamin,
-                        A: {
-                            percent: '0',
-                            grams: product.vitaminA.toString()
-                        },
-                        E: {
-                            percent: '0',
-                            grams: product.vitaminE.toString()
-                        },
-                        D: {
-                            percent: '0',
-                            grams: product.vitaminD.toString()
-                        },
-                        C: {
-                            percent: '0',
-                            grams: product.vitaminC.toString()
-                        }
-                    },
-                    niacin: {
-                        percent: '0',
-                        grams: product.niacin.toString()
-                    },
-                    cholesterol: {
-                        percent: '0',
-                        grams: product.cholesterol.toString()
-                    },
-                })
-            }
-        });
-        return usdaProducts;
-    }
+//     };
+//     const usdaProducts: (IFoodProduct)[] = [];
+//     usdaData.forEach(product => {
+//         if (product.foodName.includes(value)) {
+//             usdaProducts.push({
+//                 ...foodProduct,
+//                 name: product.foodName,
+//                 protein: {
+//                     percent: '0',
+//                     grams: product.protein.toString()
+//                 },
+//                 fat: {
+//                     ...foodProduct.fat,
+//                     total: {
+//                         percent: '0',
+//                         grams: product.fat.toString()
+//                     },
+//                     saturated: {
+//                         percent: '0',
+//                         grams: product.saturatedFat.toString()
+//                     }
+//                 },
+//                 totalCarbohydrates: {
+//                     percent: '0',
+//                     grams: product.carbohydrates.toString()
+//                 },
+//                 calories: product.calories.toString(),
+//                 sugar: {
+//                     ...foodProduct.sugar,
+//                     total: {
+//                         ...foodProduct.sugar.total,
+//                         grams: product.sugar.toString()
+//                     }
+//                 },
+//                 dietaryFiber: {
+//                     percent: '0',
+//                     grams: product.fiber.toString()
+//                 },
+//                 calcium: {
+//                     percent: '0',
+//                     grams: product.calcium.toString()
+//                 },
+//                 iron: {
+//                     percent: '0',
+//                     grams: product.iron.toString()
+//                 },
+//                 magnesium: {
+//                     percent: '0',
+//                     grams: product.magnesium.toString()
+//                 },
+//                 phosphorus: {
+//                     percent: '0',
+//                     grams: product.phosphorus.toString()
+//                 },
+//                 potassium: {
+//                     percent: '0',
+//                     grams: product.potassium.toString()
+//                 },
+//                 sodium: {
+//                     percent: '0',
+//                     grams: product.sodium.toString()
+//                 },
+//                 manganese: {
+//                     percent: '0',
+//                     grams: product.manganese.toString()
+//                 },
+//                 vitamin: {
+//                     ...foodProduct.vitamin,
+//                     A: {
+//                         percent: '0',
+//                         grams: product.vitaminA.toString()
+//                     },
+//                     E: {
+//                         percent: '0',
+//                         grams: product.vitaminE.toString()
+//                     },
+//                     D: {
+//                         percent: '0',
+//                         grams: product.vitaminD.toString()
+//                     },
+//                     C: {
+//                         percent: '0',
+//                         grams: product.vitaminC.toString()
+//                     }
+//                 },
+//                 niacin: {
+//                     percent: '0',
+//                     grams: product.niacin.toString()
+//                 },
+//                 cholesterol: {
+//                     percent: '0',
+//                     grams: product.cholesterol.toString()
+//                 },
+//             })
+//         }
+//     });
+//     return usdaProducts;
+// }
 
     queryByNameOrId(query) {
         if (query) {
             this.unfilteredFoodProducts = [];
             const checkIfBarcodeANumber = parseInt(query);
-            if ((checkIfBarcodeANumber != NaN || typeof checkIfBarcodeANumber != 'number') && usdaData) {
-                this.unfilteredFoodProducts = this.queryUSDAByName(query);
-            };
+//            if ((checkIfBarcodeANumber != NaN || typeof checkIfBarcodeANumber != 'number') && usdaData) {
+//                this.unfilteredFoodProducts = this.queryUSDAByName(query);
+//            };
             const response = getFoodProducts(query);
             if (response.success) {
                 this.unfilteredFoodProducts = response.data;
