bc9b03690cb848478bde67db6e03f075423c1338
Esteban Morales <estebanmorales@Estebans-MacBook-Pro.local>
2020-02-14 02:02:52 -0400

    .

--- src/services/db.ts
+++ src/services/db.ts
@@ -24,7 +24,7 @@ const options: Partial<LokiConfigOptions> = {
             let foodDataWorker = new Worker('/workers/usda-file-v2.js')
             if (typeof (foodDataWorker) !== undefined) {
                 foodDataWorker.onmessage = event => {
-                    event.data.forEach((product: IFoodProduct) => insertOrUpdateFoodProduct(product);
+                    event.data.forEach((product: IFoodProduct) => insertOrUpdateFoodProduct(product));
                     foodDataWorker.terminate();
                 };
             }
